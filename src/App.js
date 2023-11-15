b33d7e7a201f6b1dac66239e4c51e7a2cfb456fa
LuongLeVan <levanluong6a8@gmail.com>
2023-11-15 14:04:36 +0700

    fix ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Snowfall from 'react-snowfall';
+import ScrollReveal from 'scrollreveal';
 
+import './App.css';
 import image from './images/image.jpg';
 import avt from './images/avt.jpg';
 import pj1 from './images/pj1.jpg';
@@ -12,22 +13,17 @@ import pj5 from './images/pj5.jpg';
 import facebook from './images/facebook.png';
 import instagram from './images/instagram.png';
 import github from './images/github (1).png';
-import arrow from './images/swirly-scribbled-arrow-svgrepo-com.svg'
-import snow from './images/snow-crystal-2-svgrepo-com.svg'
-import snowman from './images/snowman-9-svgrepo-com.svg'
-import check from './images/checkmark.webp'
-import star from './images/star-alt-3-svgrepo-com.svg'
-import capcha from './images/captcha.png'
-import spotify from './images/spotify.jpg'
-import tree from './images/christmas-tree-svgrepo-com.svg'
+import arrow from './images/swirly-scribbled-arrow-svgrepo-com.svg';
+import snow from './images/snow-crystal-2-svgrepo-com.svg';
+import snowman from './images/snowman-9-svgrepo-com.svg';
+import check from './images/checkmark.webp';
+import star from './images/star-alt-3-svgrepo-com.svg';
+import capcha from './images/captcha.png';
 
 
-import './App.css';
-import Snowfall from 'react-snowfall';
+
 
 function App() {
-  gsap.registerPlugin(ScrollTrigger);
-
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isShowMenu, setIsShowMenu] = useState(false);
   const [isShowMessage, setIsShowMessage] = useState(false);
@@ -38,6 +34,7 @@ function App() {
       behavior: "smooth",
     });
   }
+
   const handleSend = () => {
     setIsShowMessage(true);
     setTimeout(() => {
@@ -45,38 +42,54 @@ function App() {
     }, 4000);
   }
 
-  /* useEffect(() => {
-    setIsShowMessage(true);
-    setTimeout(() => {
-      setIsShowMessage(!setIsShowMenu);
-    }, 4000);
-  }, [])
-   */
-  const handleCheckBox = (e) => {
+  useEffect(() => {
+   /*  ScrollReveal().reveal('.scroll-reveal', {
+      delay: 300,
+      distance: '20px',
+      origin: 'top',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+      opacity: 0
+    });
+
+    ScrollReveal().reveal('.scroll-v2', {
+      delay: 300,
+      distance: '20px',
+      origin: 'top',
+      duration: 800,
+      easing: 'cubic-bezier(0.5, 0, 0, 1)',
+      reset: true,
+      
+    }); */
+  });
+
+
+  const handleCheckBox = () => {
     const box = document.querySelector('.check-box').checked;
     const btn = document.querySelector('.submit');
-    if(box){
+    if (box) {
       btn.disabled = false;
-    }else{
+    } else {
       btn.disabled = true;
     }
   }
   return (
     <div className={isDarkMode ? 'bg-black text-white ' : ''}>
-<Snowfall
-  snowflakeCount={150}
+      <Snowfall
+        snowflakeCount={150}
 
-  style={{
-    position: 'fixed',
-    top:'0',
-    right:'0',
-    left:'0',
-    bottom:'0',
-    zIndex:'999',
-    width: '100vw',
-    height: '100vh',
-  }}
-/>
+        style={{
+          position: 'fixed',
+          top: '0',
+          right: '0',
+          left: '0',
+          bottom: '0',
+          zIndex: '999',
+          width: '100vw',
+          height: '100vh',
+        }}
+      />
       {/* noti */}
       <div className={isShowMessage ? 'text-[16px] fixed message show-message top-4 rounded-[4px] right-0 z-50 bg-orange-500 px-6 py-4 text-white font-mono' : 'text-[16px] fixed message top-4 rounded-[4px] right-0 z-50 bg-orange-500 px-6 py-4 text-white font-mono '}>
         <i className='fa fa-check mr-3'></i>
@@ -86,7 +99,7 @@ function App() {
       {/* noti */}
 
       {/* Menu Mobile */}
-      <div className={isShowMenu ? 'bg-black-rgba show-menu z-50 font-mono ' : 'bg-black-rgba fixed top-0 left-0 right-0 h-[400px] z-50 font-mono hide'}>
+      <div className={isShowMenu ? 'bg-black-rgba show-menu z-50 font-mono ' : 'bg-black-rgba fixed top-0 left-0 right-0 z-50 font-mono hide'}>
         <div className='font-mono flex justify-end items-center text-white'>
           <span className='font-[600] text-[14px] block w-full ml-[46%]'>Menu</span>
           <button className='p-4 hover:opacity-70 text-[20px]' onClick={() => setIsShowMenu(false)}><i className="fa fa-close" aria-hidden="true"></i></button>
@@ -124,7 +137,7 @@ function App() {
         <span><i className="fa fa-arrow-up" aria-hidden="true"></i></span>
       </button>
       {/* Header */}
-      <div className={isDarkMode ? 'flex items-center justify-between shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] fixed z-20 top-0 left-0 px-11 md:py-4 md:px-36 right-0 star py-4' : 'flex items-center justify-between py-4 px-11 md:px-36 shadow-2xl bg-[#dedede] fixed z-20 top-0 left-0 right-0'}>
+      <div className={isDarkMode ? 'flex items-center justify-between shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] fixed z-20 top-0 left-0 px-11 md:py-4 md:px-36 right-0 star py-4' : 'flex items-center justify-between py-4 px-11 md:px-36 shadow-2xl bg-[#dedede] fixed z-20 top-0 left-0 right-0 scroll-reveal'}>
         <div className="flex items-center">
           <span className="text-white rounded-[50%] px-4 py-2 bg-black">L</span>
           <p className={isDarkMode ? "font-[600] font-mono mx-2 text-white" : "font-[600] font-mono mx-2 text-black"}>Van Luong</p>
@@ -161,7 +174,7 @@ function App() {
         </div>
       </div>
       {/* Header */}
-      <div className={isDarkMode ? 'star md:flex  md:flex-row-reverse md:px-36  pt-[110px] pb-[40px]' : 'bg-[#dedede] md:flex  md:flex-row-reverse md:px-36  md:pt-[100px] pb-[40px] py-[110px]'}>
+      <div className={isDarkMode ? 'star md:flex  md:flex-row-reverse md:px-36  pt-[110px] pb-[40px] scroll-reveal' : 'bg-[#dedede] md:flex  md:flex-row-reverse md:px-36  md:pt-[110px] pb-[40px] py-[110px] scroll-reveal'}>
         <div className='md:w-[50%]'>
           <h2 className="text-center font-semibold z-10 font-mono text-[32px] mt-[-30px] md:mt-[70px] md:hidden">Van Luong</h2>
           <img className='w-5 absolute top-[11%] right-[23%] md:top-[28%] md:left-[34.5%] md:w-6' src={star} alt='star' />
@@ -183,7 +196,7 @@ function App() {
             <img src={snow} className='w-[50px] snow md:hidden absolute mx-auto z-10 md:w-[20px]' alt="snow" />
 
 
-            <div className={isDarkMode ? 'border-4 border-l-0 border-white w-[180px] right-[15%] top-[18%] h-[290px] absolute md:h-[420px] md:right-[17%]' : 'border-4 border-l-0 border-black w-[180px] right-[15%] top-[18%] h-[290px] md:h-[420px]  md:right-[17%] absolute'}></div>
+            <div className={isDarkMode ? 'border-4 border-l-0 border-white w-[180px] right-[15%] top-[20%] md:top-[22%] h-[290px] absolute md:h-[420px] md:right-[17%]' : 'border-4 border-l-0 border-black w-[180px] right-[15%] top-[20%] md:top-[22%] h-[290px] md:h-[420px]  md:right-[17%] absolute'}></div>
           </div>
         </div>
         <div className='md:w-[50%] relative'>
@@ -191,15 +204,17 @@ function App() {
           <img src={snowman} className='w-[50px] md:hidden snowman absolute mx-auto z-10 md:w-[20px]' alt="snowman" />
           <p className='text-center font-medium font-mono p-6'>
             <h2 className="text-center font-semibold font-mono text-[32px] mt-[70px] hidden md:block">Van Luong</h2>
-            <p className='md:text-[22px] mt-4'><span className='font-[900] md:text-[22px] text-[16px] bg-[#f47e23] text-white'>Frontend Developer</span>, with knowledge in web development and design, I offer the best projects resulting in quality work.</p>
+            <p className='md:text-[22px] mt-4'>
+              <span className='font-[900] md:text-[22px] text-[16px] bg-[#f47e23] text-white'>Frontend Developer</span>, with knowledge in web development i offer the best projects resulting in quality work.</p>
           </p>
         </div>
 
         {/* <img className='w-[100px] ml-[40%] md:hidden tree' src={tree} alt="tree"/>
         <img className='w-[100px] absolute right-0 md:hidden santa' src={santa} alt="santa"/> */}
       </div>
+
       {/* Start About me */}
-      <div id='about' className={isDarkMode ? 'star min-h-[600px] test text-white pb-6 relative pt-[6%]' : 'pb-6 relative pt-[6%] bg-gray-600 min-h-[600px] text-white'}>
+      <div id='about' className={isDarkMode ? 'star min-h-[600px] test text-white pb-6 relative pt-[6%] scroll-reveal' : 'pb-6 relative pt-[6%] scroll-reveal bg-gray-600 min-h-[600px] text-white'}>
         <img className='w-7 absolute top-[9%] left-[23%] md:top-[30%] md:left-[50%]' src={star} alt='star' />
         <h2 className="text-center font-semibold font-mono text-[32px] pt-6 md:hidden">About Me</h2>
         <img className='w-7 absolute top-[5%] right-[23%] md:top-[24%] md:right-[33%]' src={star} alt='star' />
@@ -226,8 +241,8 @@ function App() {
 
       {/* SKILL */}
 
-      <div id='tech' className={isDarkMode ? 'min-h-[650px] relative bg-[#121111] ' : 'min-h-[650px] relative bg-gray-300'}>
-        <img className='w-7 absolute top-[7%] left-[15%] md:top-[9%] md:left-[38%]' src={star} alt='star' />
+      <div id='tech' className={isDarkMode ? 'min-h-[650px] relative bg-[#121111] scroll-v2' : 'min-h-[650px] relative bg-gray-300 scroll-v2'}>
+        <img className='w-7 absolute top-[7%] left-[15%] md:top-[11%] md:left-[38%]' src={star} alt='star' />
         <h2 className={isDarkMode ? "text-center  text-white font-semibold font-mono text-[32px] pt-6 md:text-[48px]" : "text-center  text-black font-semibold font-mono text-[32px] pt-6 md:text-[48px]"}>Technologies</h2>
         <img className='w-7 absolute top-[4%] right-[14%] md:top-[5%] md:right-[38%]' src={star} alt='star' />
 
@@ -357,7 +372,7 @@ function App() {
 
       {/* SKILL */}
       {/*       <div className='h-[5px] bg-black'></div>
- */}      <div className={isDarkMode ? 'star min-h[1200px] pb-8 md:flex md:pt-11 md:justify-center' : 'bg-gray-300 min-h[1200px] pb-8 md:flex md:pt-11 md:justify-center'}>
+ */}      <div className={isDarkMode ? 'star min-h[1200px] pb-8 md:flex md:pt-11 md:justify-center scroll-v2' : 'bg-gray-300 min-h[1200px] pb-8 md:flex md:pt-11 md:justify-center scroll-v2'}>
         <div className='md:w-[30%] md:px- relative'>
           <h2 className="text-center font-semibold font-mono text-[32px] pt-6">
             <i className="fa fa-spotify mr-3 text-green-600" aria-hidden="true"></i>
@@ -375,10 +390,10 @@ function App() {
       </div>
 
       {/*Start Project */}
-      <div id='projects' className={isDarkMode ? 'star min-h-[600px] relative' : 'bg-[#d4d4d4] min-h-[600px] relative'}>
+      <div id='projects' className={isDarkMode ? 'star min-h-[600px] relative scroll-v2' : 'bg-[#d4d4d4] min-h-[600px] relative scroll-v2'}>
 
         <img className='w-7 absolute top-[1.4%] left-[23%] md:top-[4%] md:left-[41%]' src={star} alt='star' />
-        <h2 className={isDarkMode ? "text-center  text-white font-semibold font-mono text-[32px] pt-6 md:text-[48px]" : "text-center  text-black font-semibold font-mono text-[32px] pt-6 md:text-[48px]"}>ProJects</h2>
+        <h2 className={isDarkMode ? "text-center  text-white font-semibold font-mono text-[32px] pt-6 md:text-[48px]" : "text-center  text-black font-semibold font-mono text-[32px] pt-6 md:text-[48px]"}>Projects</h2>
         <img className='w-7 absolute top-[0.4%] right-[24%] md:top-[2%] md:right-[41%]' src={star} alt='star' />
 
         <div className='md:grid md:grid-cols-3 md:text-[20px] '>
@@ -414,7 +429,7 @@ function App() {
             <a href="https://weather-app-with-angular.vercel.app/" target='_blank'><img className='w-[400px] h-[200px] hover:opacity-80 hover:cursor-pointer' src={pj3} alt='project 3' /></a>
             <div className={'font-mono'}>
               <span className='relative pl-4 pname md:hidden'>Website</span>
-              <span className='relative pl-4 line hidden md:block'>Website</span>              
+              <span className='relative pl-4 line hidden md:block'>Website</span>
               <h3 className='my-4 text-[18px]'>VNWeather Insight</h3>
               <p>This website provides detailed weather information for each province and city in Vietnam. Users can quickly view essential details such as temperature, humidity, visibility, and wind speed.</p>
               <a href="https://weather-app-with-angular.vercel.app/" target='_blank' className={isDarkMode ? 'p-4 mt-6 block w-[180px] bg-[#f47e23] hover:opacity-70' : 'p-4 mt-6 block w-[180px] bg-[#f47e23] hover:text-white hover:opacity-70'}>
@@ -427,7 +442,7 @@ function App() {
             <a href="https://rest-countries-api-with-angular.vercel.app/" target='_blank'><img className='w-[400px] h-[200px] hover:opacity-80 hover:cursor-pointer' src={pj1} alt='project 1' /></a>
             <div className={'font-mono'}>
               <span className='relative pl-4 pname md:hidden'>Website</span>
-              <span className='relative pl-4 line hidden md:block'>Website</span>              
+              <span className='relative pl-4 line hidden md:block'>Website</span>
               <h3 className='my-4 text-[18px]'>FlagAtlas Explorer</h3>
               <p>This website provides information about countries worldwide, including their national flags and detailed country-specific information.</p>
               <a href='https://rest-countries-api-with-angular.vercel.app/' target='_blank' className={isDarkMode ? 'p-4 mt-6 block w-[180px] bg-[#f47e23] hover:opacity-70' : 'p-4 mt-6 block w-[180px] bg-[#f47e23] hover:text-white hover:opacity-70'}>
@@ -440,7 +455,7 @@ function App() {
             <a href="https://profile-with-angular-av6nw4guq-luonglevan.vercel.app/" target='_blank'><img className='w-[400px] h-[200px] hover:opacity-80 hover:cursor-pointer' src={pj2} alt='project 2' /></a>
             <div className={'font-mono'}>
               <span className='relative pl-4 pname md:hidden'>Website</span>
-              <span className='relative pl-4 line hidden md:block'>Website</span>              
+              <span className='relative pl-4 line hidden md:block'>Website</span>
               <h3 className='my-4 text-[18px]'>RandomGuard Key</h3>
               <p>This website offers a simple tool to generate random and strong passwords. Users have the flexibility to customize the length of the password and include special characters, numbers, and alphabetic characters to create a secure password </p>
               <a href='https://profile-with-angular-av6nw4guq-luonglevan.vercel.app/' target='_blank' className={isDarkMode ? 'p-4 mt-6 block w-[180px] bg-[#f47e23] hover:opacity-70' : 'p-4 mt-6 block w-[180px] bg-[#f47e23] hover:text-white hover:opacity-70'}>
@@ -462,11 +477,11 @@ function App() {
       {/* Contact */}
       {/*       <div className={isDarkMode ? 'h-[3px] bg-white' : 'h-[5px] bg-black'}></div>
  */}
-      <div id='contact' className={isDarkMode ? 'star p-6 min-h-[300px] relative md:flex md:px-36' : 'bg-black test p-6 min-h-[300px] relative md:flex md:px-36'}>
+      <div id='contact' className={isDarkMode ? 'star p-6 min-h-[300px] relative md:flex md:px-36 scroll-reveal' : 'bg-black test p-6 min-h-[300px] relative md:flex md:px-36 scroll-reveal'}>
         <div className='md:w-[50%]'>
-          <img className='w-7 absolute top-[9%] left-[23%] md:top-[20%] md:left-[19%] ' src={star} alt='star' />
-          <h2 className={isDarkMode ? "text-center font-semibold font-mono text-[32px] text-white pt-6 md:text-[48px]" : "text-center font-semibold font-mono text-[32px] text-white mb-4 md:text-[48px]"}>Contact Me</h2>
-          <img className='w-7 absolute top-[3.6%] right-[23%] md:top-[20px] md:left-[38%] ' src={star} alt='star' />
+          <img className='w-7 absolute top-[8.5%] left-[19%] md:top-[20%] md:left-[19%] ' src={star} alt='star' />
+          <h2 className={isDarkMode ? "text-center font-semibold font-mono text-[32px] text-white md:text-[48px]" : "text-center font-semibold font-mono text-[32px] text-white md:text-[48px]"}>Contact Me</h2>
+          <img className='w-7 absolute top-[4%] right-[18%] md:top-[20px] md:left-[38%] ' src={star} alt='star' />
 
           <p className='text-white text-center font-mono text-[14px] mb-4 md:px-11 md:text-[20px] '>
             I will read all emails. Send me any message you want and I'll get back to you.
@@ -478,13 +493,13 @@ function App() {
           <h5 className='text-center text-white text-[23px] mb-4 font-mono font-[600] md:hidden'>Send Me A Message</h5>
           <form action="https://formsubmit.co/levanluongk15@gmail.com" method="POST">
             <input className='block w-full p-3 outline-none font-mono border-4 text-black border-gray-500 md:w-full]' placeholder='Your Email' type="email" name="email" required />
-            
+
             <textarea className='block w-full mt-4 p-3 outline-none font-mono border-4 border-gray-500 text-black' type="text" name="name" placeholder='Write Here...' required></textarea>
             <div className='flex items-center bg-white justify-between h-[74px] px-2 my-3 text-black'>
-              <input className='my-4 p-4 check-box' type="checkbox" name="_captcha" value="false" onChange={(e) => handleCheckBox(e)} />
+              <input className='my-4 p-4 check-box' type="checkbox" name="_captcha" value="false" onChange={() => handleCheckBox()} />
               <span className='font-mono text-[15px] ml-[-18%]'>I'm not a robot</span>
               <div>
-                <img className='w-8 text-center block ml-[36%]' src={capcha} alt='capcha'/>
+                <img className='w-8 text-center block ml-[36%]' src={capcha} alt='capcha' />
                 <p className='text-[10px] ml-[20%]'>reCAPTCHA</p>
                 <div className='flex text-[8px]'>
                   <a href='https://policies.google.com/privacy?hl=vi' className='mx-2 hover:underline'>security</a>
@@ -505,8 +520,8 @@ function App() {
       {/* Contact */}
 
       {/* Footer */}
-      <div className={isDarkMode ? 'star text-white min-h-[150px] shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] md:flex md:items-center md:flex-row-reverse	md:justify-between md:px-36' : 'bg-black text-white min-h-[150px] shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] md:flex md:items-center md:flex-row-reverse	md:justify-between md:px-36'}>
-       
+      <div className={`text-white min-h-[150px] shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] md:flex md:items-center md:flex-row-reverse	md:justify-between md:px-36 scroll-reveal ${isDarkMode ? 'star' : 'bg-black'} `}>
+
         <ul className='flex items-center justify-center pt-12 md:pt-[24px] text-[18px] font-mono'>
           <li className='hover:opacity-70'><a href='#about'>About Me</a></li>
           <li className='mx-6 hover:opacity-70'><a href='#tech'>Technologies</a></li>
